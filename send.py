a1581e81e5dd8ad7dfd771608642c43780efb562
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-06-08 09:33:57 +0530

    Update send.py

--- send.py
+++ send.py
@@ -2,18 +2,15 @@
 from twilio.rest import Client 
 
 # Your Account Sid and Auth Token from twilio.com / console 
-account_sid = 'AC057dcec475148e15b942f27275acab48'
-auth_token = '20b8dca75f53aa2f17471f44d8dcf2de'
+account_sid = 'PASTE YOUR OWN SID BY REGISTERING AT TWILIO'
+auth_token = 'PASTE YOUR OWN TOKEN BY REGISTERING AT TWILIO'
 
 client = Client(account_sid, auth_token) 
 
-''' Change the value of 'from' with the number 
-received from Twilio and the value of 'to' 
-with the number in which you want to send message.'''
 message = client.messages.create( 
-							from_="+12105298640", 
-							body ="EDEN HAZARD JOINS REAL MADRID!!", 
-							to ="+917003896831"
+							from_="REPLACE WITH TWILIO TRIAL/PAID NUMBER", 
+							body =" (SAMPLE MESSAGE) EDEN HAZARD JOINS REAL MADRID!!", 
+							to ="THE NUMBER WHERE YOU WANT TO SEND SMS TO"
 						) 
 
 print(message.sid) 
